6281b726f6bb11d2c28c87f37c3e19f579ac4007
lsahatqija <leke.sahatqija@live.com>
2016-02-14 22:29:06 +0100

    sprites update

--- src/pizzatales/StartingClass.java
+++ src/pizzatales/StartingClass.java
@@ -20,7 +20,7 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 641656516622083167L;
-	public static int difficultylevel = 4;
+	public static int difficultylevel = 2;
 	private static Player player;
 	private Image image, character1, character2, characterMove1, characterMove2, currentSprite, background;
 	public static Image tileTree, tileGrass, tileWall;
